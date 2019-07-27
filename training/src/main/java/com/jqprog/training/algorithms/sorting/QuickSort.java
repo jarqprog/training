bac7ed05bafd064fb5ee78dd75ba797a7ae450b6
jarqprog <jarqprog@gmail.com>
2019-07-27 22:57:52 +0200

    add quick sort implementation

--- training/src/main/java/com/jqprog/training/algorithms/sorting/QuickSort.java
+++ training/src/main/java/com/jqprog/training/algorithms/sorting/QuickSort.java
@@ -2,18 +2,69 @@ package com.jqprog.training.algorithms.sorting;
 
 public class QuickSort implements Sort {
 
-    @Override
-    public int[] sort(int[] numbers) {
-        return new int[0];
-    }
+    private long performedOperations;
+    private long lastExecutionTime;
+    private int[] array;
+
 
     @Override
     public long getPerformedOperations() {
-        return 0;
+        return performedOperations;
     }
 
     @Override
     public long getLastExecutionTimeInNanos() {
-        return 0;
+        return lastExecutionTime;
+    }
+
+    @Override
+    public int[] sort(int[] numbers) {
+        long startTime = System.nanoTime();
+
+        lastExecutionTime = 0;
+        performedOperations = 0;
+        array = numbers;
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        sortRecursive(left, right);
+
+        lastExecutionTime = System.nanoTime() - startTime;
+        return numbers;
+    }
+
+
+    private void sortRecursive(int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(left, right);
+            sortRecursive(left, partitionIndex-1);
+            sortRecursive(partitionIndex+1, right);
+        }
+    }
+
+    private int partition(int left, int right) {
+
+        int pivot = array[right];
+        int borderIndex = left;
+        for (int i=left; i<right; i++) {
+            performedOperations++;
+
+            if (array[i] < pivot) {
+                swap(i, borderIndex);
+                borderIndex++;
+            }
+        }
+        swap(right, borderIndex);
+        return borderIndex;
+    }
+
+    private void swap(int firstIndex, int secondIndex) {
+        if (firstIndex == secondIndex) {
+            return;
+        }
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
     }
 }
