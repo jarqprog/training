ba51183f62284e2c2d6cfe439020a9ba50aeac3b
jarqprog <jarqprog@gmail.com>
2019-07-27 12:06:27 +0200

    remove constructor with Number[] parameter, remove throwing IllegalArgumentException and message field

--- training/src/main/java/com/jqprog/training/algorithms/model/IntNumbers.java
+++ training/src/main/java/com/jqprog/training/algorithms/model/IntNumbers.java
@@ -6,17 +6,12 @@ import java.util.Collection;
 public class IntNumbers implements Numbers {
 
     private final int[] numbers;
-    private final String illegalArgExceptionMessage = "Numbers type is incorrect!";
 
     public IntNumbers(int[] numbers) {
         this.numbers = numbers;
     }
 
-    public IntNumbers(Number[] numbers) {
-        this.numbers = transformToIntArray(numbers);
-    }
-
-    public IntNumbers(Collection<Number> numbers) {
+    public IntNumbers(Collection<Integer> numbers) {
         this.numbers = transformToIntArray(numbers);
     }
 
@@ -35,30 +30,13 @@ public class IntNumbers implements Numbers {
         return Arrays.toString(this.numbers);
     }
 
-    private int[] transformToIntArray(Collection<Number> numbers) throws IllegalArgumentException {
+    private int[] transformToIntArray(Collection<Integer> numbers) {
         int len = numbers.size();
         int[] nums = new int[len];
         int index = 0;
         for (Number num : numbers) {
-            try {
                 nums[index] = (int) num;
                 index++;
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException(illegalArgExceptionMessage);
-            }
-        }
-        return nums;
-    }
-
-    private int[] transformToIntArray(Number[] numbers) throws IllegalArgumentException {
-        int len = numbers.length;
-        int[] nums = new int[len];
-        for (int i=0; i<len; i++) {
-            try {
-                nums[i] = (int) numbers[i];
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException(illegalArgExceptionMessage);
-            }
         }
         return nums;
     }
