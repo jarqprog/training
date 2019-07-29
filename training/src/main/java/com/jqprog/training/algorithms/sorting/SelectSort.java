41088ed4756548a835bf6682e120a9730e0fccd6
jarqprog <jarqprog@gmail.com>
2019-07-29 07:52:07 +0200

    add select sort implementation

--- training/src/main/java/com/jqprog/training/algorithms/sorting/SelectSort.java
+++ training/src/main/java/com/jqprog/training/algorithms/sorting/SelectSort.java
@@ -2,7 +2,6 @@ package com.jqprog.training.algorithms.sorting;
 
 public class SelectSort implements Sort {
 
-
     private long performedOperations;
     private long lastExecutionTime;
 
@@ -19,13 +18,30 @@ public class SelectSort implements Sort {
     @Override
     public int[] sort(int[] numbers) {
         long startTime = System.nanoTime();
-
         lastExecutionTime = 0;
         performedOperations = 0;
 
+        int len = numbers.length;
 
+        for (int i=0; i<len; i++ ) {
+            for (int j=i; j<len; j++) {
+                performedOperations++;
+                if (numbers[j] < numbers[i]) {
+                    swap(numbers, j, i);
+                }
+            }
+        }
 
+        lastExecutionTime = System.nanoTime() - startTime;
         return numbers;
     }
 
+    private void swap(int[] numbers, int firstIndex, int secondIndex) {
+        if (firstIndex == secondIndex) {
+            return;
+        }
+        int temp = numbers[firstIndex];
+        numbers[firstIndex] = numbers[secondIndex];
+        numbers[secondIndex] = temp;
+    }
 }
